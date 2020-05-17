5aec19556e9056e7d0bb7978387288f03d88ef04
prasanna <prasan5984@gmail.com>
2020-05-17 13:08:50 +0530

    Include storage details

--- NotifyCrashLog.go
+++ NotifyCrashLog.go
@@ -1,10 +1,19 @@
 package b2c
 
 import (
+	"context"
 	"fmt"
 	"net/http"
+
+	"cloud.google.com/go/storage"
+	"google.golang.org/api/option"
 )
 
 func NotifyCrashLog(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello World!")
+	ctx := context.Background()
+	client, _ := storage.NewClient(ctx, option.WithoutAuthentication())
+	bkt := client.Bucket("crash-logs")
+	obj := bkt.Object("crash-file-1")
+	storeWriter := obj.NewWriter(ctx)
+	fmt.Fprintf(storeWriter, "Test data\n")
 }
